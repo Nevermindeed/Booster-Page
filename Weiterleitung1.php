752abaa7be7348bd1288a0d00c2061225088ac50
Dennis Droße <90307987+Nevermindeed@users.noreply.github.com>
2021-11-26 09:54:03 +0100

    merge

---
+++ Weiterleitung1.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>About Us</title>
+    <link rel="stylesheet" href="Weiterleitung1.css" type="text/css">
+</head>
+
+<body>
+    <h1>Die Coaches stellen sich vor</h1>
+    <input type="checkbox" id="click"><label for="click" class="click-me">Profile</label>
+    <div class="content">
+        <div class="header">
+            <h2>Coach:Dennis</h2>
+            <label for="click" class="times"></label>
+        </div>
+        <label for="click" class="check"></label>
+        <a> href="https: //www.leagueofgraphs.com/de/summoner/euw/Nevermindeed"</a>
+        <div class="line"></div>
+        <label for="click" class="close-button">Close</label>
+
+    </div>
+    </div>
+</body>
+
+</html>
